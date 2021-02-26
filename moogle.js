5b72c76e47d9e6b0de7ad565bf38b2514df82f6a
Kerrance <hello@kerrisharp.com>
2021-02-26 08:22:44 +0000

    Show pop-up modal when scrolling 60% of the page height

--- moogle.js
+++ moogle.js
@@ -9,6 +9,8 @@
   const popup = document.getElementById('kupo');
   const showPopupButton = document.getElementById('kupo__open');
   const closePopupButton = document.getElementById('kupo__close');
+  const rootElement = document.documentElement;
+  const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
 
   function showPopup() {
     popup.classList.add('kupo__active');
@@ -18,11 +20,18 @@
     popup.classList.remove('kupo__active');
   }
 
+  function showPopupOnScroll() {
+    if ((rootElement.scrollTop / scrollTotal) > 0.6) {
+      showPopup();
+    }
+  }
+
   function eventListener() {
     showPopupButton.addEventListener('click', showPopup);
     closePopupButton.addEventListener('click', hidePopup);
+    document.addEventListener('scroll', showPopupOnScroll);
 
-    document.onclick = function(event) {
+    rootElement.onclick = function(event) {
       if (event.target === popup) {
         hidePopup();
       }
